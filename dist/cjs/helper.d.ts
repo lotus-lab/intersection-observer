589dd2e406f6036b4f5cd245b1d2b1c3222ae6ee
Humed-Muhammad <abc45171@gmail.com>
2022-10-31 09:13:50 +0300

    🚑 HOTFIX:

--- dist/cjs/helper.d.ts
+++
@@ -1,5 +0,0 @@
-import { IntersectionCallback, OptionsType } from './types';
-export declare const checkRootObject: (options: OptionsType) => any;
-export declare const updateAdTimer: (adBox: HTMLElement) => void;
-export declare const handleVisibilityChange: () => void;
-export declare const intersectionCallback: ({ entries, inViewStateForObserveOnce, observerOptions, observer, setInViewData, }: IntersectionCallback) => void;
--- lib/cjs/index.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-import { OptionsType } from './types';
-export declare const useInViewTrigger: (options?: OptionsType) => {
-    visibilityTime?: number | undefined;
-    inView: boolean;
-    entry?: IntersectionObserverEntry | null | undefined;
-    targetRef: import("react").MutableRefObject<any>;
-};
